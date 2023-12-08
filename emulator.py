dd5a76e48e1cf76e054a4f2d4f1eec20e3924748
Ben Wurster <wursterben17@gmail.com>
2023-12-07 20:21:45 -0600

    there are no accidents

--- emulator.py
+++ emulator.py
@@ -25,10 +25,9 @@ def readtopology(topology_file):
     while len(curr_line) != 0:
         neigh_parts = curr_line.split(" ")
         route_topology[neigh_parts[0]] = []
-        for i in range(1, len(neigh_parts)):
-            if i == len(neigh_parts)-1:
-                neigh_parts[i] = neigh_parts[i].strip()
-            route_topology[neigh_parts[0]].append(neigh_parts[i])
+        for part in neigh_parts[1:]:
+            s_part = part.strip()
+            route_topology[neigh_parts[0]].append(s_part)
         curr_line = topology_file.readline()
 
     return route_topology
@@ -40,70 +39,70 @@ def forwardpacket():
     return 0
 
 def buildForwardTable(route_topology, start_node):
-
     num_nodes = len(route_topology)
 
     # once we find a next hop it stays the same
     
-    confirmed_list = []
+    confirmed_list = {}
     # start by adding start node with cost of 0 and no next node
-    confirmed_list.append((start_node, 0, -1))
+    confirmed_list[start_node] =  (0, -1)
 
     # look at LSP of this node (immediate neighbors) and put all its neigbors inthe tentative list
-    neighs = route_topology[confirmed_list[0][0]]
+    root_neighs = route_topology[start_node]
     tentative_list = []
-    for neigh in neighs:
-        tentative_list.append((neigh, confirmed_list[0][1]+1, neigh))
+    for neigh in root_neighs:
+        tentative_list.append((neigh, 1, neigh))
 
-    while (len(confirmed_list) != num_nodes):
+    while (len(tentative_list) > 0):
+        next_node = tentative_list.pop(0)
+        next_neighs = route_topology[next_node[0]]
 
-        # put lowest cost member of tentative list into confirmed list
-        lowest_cost = 9999
-        popIndex = -1
-        chosen_node = tentative_list[0]
-        for pos_node in range(len(tentative_list)):
-            if tentative_list[pos_node][1] < lowest_cost:
-                lowest_cost = tentative_list[pos_node][1]
-                chosen_node = tentative_list[pos_node]
-                popIndex = pos_node
+        for neigh in next_neighs:
+            if not (neigh in confirmed_list or neigh in [x[0] for x in tentative_list]):
+                tentative_list.append((neigh, next_node[1] + 1, next_node[2]))
 
-        confirmed_list.append(chosen_node)
-        tentative_list.pop(popIndex)
-
-        if (len(confirmed_list) == num_nodes):
-            break
-
-        # examine the neighbors of the newly confirmed member
-        neighs = neighs = route_topology[confirmed_list[-1][0]]
-
-        tentative_list = []
-        for neigh in neighs:
-            tentative_list.append((neigh, confirmed_list[-1][1]+1, confirmed_list[-1][2]))
+        confirmed_list[next_node[0]] = (next_node[1], next_node[2])
 
     return confirmed_list
 
+
+def link_nodes(route_topology, id_1, id_2):
+    # handle one direction
+    if id_1 in route_topology:
+        if not id_2 in route_topology[id_1]:
+            route_topology[id_1].append(id_2)
+    else:
+        route_topology[id_1] = [id_2]
+
+    # handle other direction
+    if id_2 in route_topology:
+        if not id_1 in route_topology[id_2]:
+            route_topology[id_2].append(id_1)
+    else:
+        route_topology[id_2] = [id_1]
+
+    return route_topology
     
+def unlink_nodes(route_topology, id_1, id_2):
+    if id_1 in route_topology:
+        if id_2 in route_topology[id_1]:
+            route_topology[id_1].remove(id_2)
 
-def change_topology_add(initial_route_topology, route_topology, src_ip, src_port):
-    for node in route_topology:
-        # the received packet is not currently listed as a neighbor but it was a neighbor in the initial configuration
-        if str(src_ip + "," + src_port) not in route_topology[node] and str(src_ip + "," + src_port) in initial_route_topology[node]:
-            route_topology[node].append(str(src_ip + "," + src_port))
-
-    # get all the keys
-    all_keys = route_topology.keys()
-
-    # reconstruct key for unavailable node
-    route_topology[str(src_ip + "," + src_port)] = []
-
-    # only want to append keys that are listed as a neighbor in the initial configuration
-    for key in all_keys:
-        if key in initial_route_topology[str(src_ip + "," + src_port)]:
-            route_topology[str(src_ip + "," + src_port)].append(key)
+    if id_2 in route_topology:
+        if id_1 in route_topology[id_2]:
+            route_topology[id_2].remove(id_1)
 
     return route_topology
 
-def change_topology_remove(node_to_remove, route_topology):
+def change_topology_add(route_topology, base_ip, base_port, new_ip, new_port):
+    base_id = str(base_ip + "," + base_port)
+    new_id = str(new_ip + "," + new_port)
+    
+    route_topology = link_nodes(route_topology, base_id, new_id)
+
+    return route_topology
+
+def change_topology_remove(route_topology, node_to_remove):
     # removing the node from each node's list of neighbors if it contains it
     for node in route_topology:
         if node_to_remove in route_topology[node]:
@@ -114,6 +113,29 @@ def change_topology_remove(node_to_remove, route_topology):
         del route_topology[node_to_remove]
 
     return route_topology
+
+def check_and_update_topology(route_topology, base_id, neighbors):
+    hasChanged = False
+
+    if base_id in route_topology:
+        if not sorted(neighbors) == sorted(route_topology[base_id]):
+            hasChanged = True
+            # items in neighbors not in topology (new neighbors)
+            for neighbor in [neighbor for neighbor in neighbors if neighbor not in route_topology[base_id]]:
+                route_topology = link_nodes(route_topology, neighbor, base_id)
+            # items in topology not in neighbors (lost neighbors)
+            for neighbor in [neighbor for neighbor in route_topology[base_id] if neighbor not in neighbors]:
+                route_topology = change_topology_remove(route_topology, neighbor)
+            # update base_id neighbors
+            route_topology[base_id] = neighbors
+    else:
+        route_topology[base_id] = neighbors
+        for neighbor in neighbors:
+            link_nodes(route_topology, neighbor, base_id)
+        hasChanged = True
+
+    return (route_topology, hasChanged)
+
 
 def main():
 
@@ -136,30 +158,22 @@ def main():
         print("A file error has occurred.")
         return 1
     
+    # calculate initial route topology
     route_topology = readtopology(tracker_file)
 
-    initial_route_topology = route_topology
-
-    print(initial_route_topology)
-
     # pass in route topology into buildForwardTable to find the forwarding table
+    forwarding_table = buildForwardTable(route_topology=route_topology, start_node=str(this_ip_addr + "," + args.p))
+    print(forwarding_table)
 
     # get the neighbors of the emulators we are currently on
     neighs = route_topology[str(this_ip_addr + "," + args.p)]
 
-    latest_timestamps = []
+    latest_timestamps = {}
     # latest timestamps set to current time
     for neigh in neighs:
-        latest_timestamps.append((neigh, time.time()))
+        latest_timestamps[neigh] = time.time()
 
-    largest_seqnumbers = []
-    all_keys = route_topology.keys()
-
-    # we need the largest sequence number of the received LinkStateMessages from each node except itself.
-    for key in all_keys:
-        if key != str(this_ip_addr + "," + args.p):    
-            largest_seqnumbers.append([key, -1])
-
+    largest_seqnumbers = {}
 
     # Receive packet from network in a non-blocking way. This means that you should not wait/get blocked in the recvfrom function until you get a packet.
     socket_obj.setblocking(False)
@@ -177,7 +191,7 @@ def main():
             full_packet = None
 
         if full_packet:
-            
+
             # If it is a helloMessage, your code should...
             if full_packet[0] == b'H':
                 # unpack type, ip, and port
@@ -193,12 +207,13 @@ def main():
                 # Check the route topology stored in this emulator. If the sender of helloMessage is from a previously unavailable node, change the route topology and forwarding table stored in this emulator. Then generate and send a new LinkStateMessage to its neighbors.
                 if str(src_ip + "," + src_port) not in route_topology:
                     # update route topology by using initial topology to help reconstruct the routes
-                    route_topology = change_topology_add(initial_route_topology, route_topology, src_ip, src_port)
+                    route_topology = change_topology_add(route_topology, this_ip_addr, args.p, src_ip, src_port)
 
                     # need new forwarding table based on this new topology
-                    # forwarding_table = buildForwardTable(route_topology, str(this_ip_addr + "," + args.p))
+                    forwarding_table = buildForwardTable(route_topology, str(this_ip_addr + "," + args.p))
 
                     # generate and send new link state message to neighbors
+                    # TODO
 
             # If it is a LinkStateMessage, your code should...
             elif full_packet[0] == b'L':
@@ -209,24 +224,29 @@ def main():
                 seq_num = header_no_neigh[2]
                 ttl = header_no_neigh[3]
 
-                # Check the largest sequence number of the sender node to determine whether it is an old message. If it’s an old message, ignore it. 
-                if seq_num < largest_seqnumbers[str(base_ip + "," + base_port)][1]:
-                    continue
+                neighbors = []
+                neighbors_raw = full_packet[14:]
+                num_neighbors = (len(full_packet) - 14) // 10
+                for i in range(num_neighbors):
+                    neighbor_struct = full_packet[14*i:14*i+10]
+                    neighbor_info = struct.unpack("!LHL", neighbor_struct)
+                    neighbors.append(str(neighbor_info[0] + "," + neighbor_info[1]))
+
+                # Check the largest sequence number of the sender node to determine whether it is an old message. If it’s an old message, ignore it. If not in record, add to record.
+                base_id = str(base_ip + "," + base_port)
+                if base_id in largest_seqnumbers:
+                    if seq_num <= largest_seqnumbers[base_id]:
+                        continue # TODO this is concerning
+                    else:
+                        largest_seqnumbers[base_id] = seq_num
+                else:
+                    largest_seqnumbers[base_id] = seq_num
 
                 # If the topology changes, update the route topology and forwarding table stored in this emulator if needed.
-                if str(base_ip + "," + base_port) not in route_topology:
-                    route_topology = change_topology_add(initial_route_topology, route_topology, src_ip, src_port)
+                route_topology, hasChanged = check_and_update_topology(route_topology, base_id, neighbors)
 
-                    # update largest timestamp since it was used
-                    for tuple in largest_seqnumbers:
-                        if tuple[0] == str(base_ip + "," + base_port):
-                            # find index of current tuple
-                            index = largest_seqnumbers.index(tuple)
-                            # update timestamp for this neighbor
-                            largest_seqnumbers[index][1] = seq_num
-
-                    # need new forwarding table based on this new topology
-                    forwarding_table = buildForwardTable(route_topology)
+                # need new forwarding table based on this new topology
+                forwarding_table = buildForwardTable(route_topology)
 
                 # Call forwardpacket function to make a process of flooding the LinkStateMessage to its own neighbors.
                 # forwardpacket()
@@ -269,97 +289,6 @@ def main():
                     # do later
 
             # Send the newest LinkStateMessage to all neighbors if the defined intervals have passed.
-
-    
-
-
-    #     if full_packet:
-    #         # unpack packet information
-    #         header = full_packet[:17]
-    #         header = struct.unpack("!BLHLHI", ip_header)
-    #         data = full_packet[26:]
-
-    #         priority = ip_header[0]
-    #         src_addr = ip_header[1]
-    #         src_addr = socket.inet_ntoa(struct.pack("!L", src_addr))
-    #         src_port = ip_header[2]
-    #         dst_addr = ip_header[3]
-    #         dst_addr = socket.inet_ntoa(struct.pack("!L", dst_addr))
-    #         dst_port = ip_header[4]
-    #         length_udp_and_data = ip_header[5]
-    #         packet_type = udp_header[0]
-    #         seq_num = udp_header[1]
-    #         payload_length = udp_header[2]
-
-    #         # if (packet_type == b'A'):
-    #         #     print(f"[EMULATOR] Recieved ACK for SEQ_NUM {socket.ntohl(seq_num)}")
-    #         # elif (packet_type == b'D'):
-    #         #     print(f"[EMULATOR] Recieved DATA for SEQ_NUM {socket.ntohl(seq_num)}")
-    #         # elif (packet_type == b'R'):
-    #         #     print(f"[EMULATOR] Recieved REQUEST")
-
-
-    #         # not valid address
-    #         if not link_params:
-    #             with open(log, 'a') as lf:
-    #                 lf.write(f"[{time.asctime()}] SRC: {src_addr}:{src_port}, DST: {dst_addr}:{dst_port}, Priority: {priority}, Size: {payload_length}, No forwarding entry found.\n")
-    #         # add to queue if not full
-    #         else:
-    #             # print("Queue length:", len(queue[priority-1]))
-    #             if (len(queue[priority-1]) >= queue_size):
-    #                 # send all end packets if they don't enter queue
-    #                 if(packet_type == 'E'):
-    #                     # send directly to emulator
-    #                     socket_obj.sendto(full_packet, (socket.gethostbyname(link_params[4]), int(link_params[5])))
-    #                 with open(log, 'a') as lf:
-    #                     lf.write(f"[{time.asctime()}] SRC: {src_addr}:{src_port}, DST: {dst_addr}:{dst_port}, Priority: {priority}, Size: {payload_length}, Priority queue {priority} was full.\n")
-    #             #add packet to queue
-    #             else:
-    #                 queue[priority-1].append(full_packet)
-        
-    #     if packet:
-    #         # get packet info
-    #         ip_header = packet[:17]
-    #         ip_header = struct.unpack("!BLHLHI", ip_header)
-    #         udp_header = packet[17:26]
-    #         udp_header = struct.unpack("!cII", udp_header)
-    #         data = packet[26:]
-    #         priority = ip_header[0]
-    #         src_addr = ip_header[1]
-    #         src_addr = socket.inet_ntoa(struct.pack("!L", src_addr))
-    #         src_port = ip_header[2]
-    #         dst_addr = ip_header[3]
-    #         dst_addr = socket.inet_ntoa(struct.pack("!L", dst_addr))
-    #         dst_port = ip_header[4]
-    #         length_udp_and_data = ip_header[5]
-    #         packet_type = udp_header[0]
-    #         seq_num = udp_header[1]
-    #         payload_length = udp_header[2]
-
-    #         # get link parameters based on packet info
-    #         send_point = (dst_addr, dst_port)
-    #         link_params = static_forwarding_lookup.get(send_point)
-
-    #         # if there is no valid link available, remove packet from queue and log event
-    #         if (not link_params):
-    #             queue[priority-1].pop(0)
-    #             with open(log, 'a') as lf:
-    #                 lf.write(f"[{time.asctime()}] SRC: {src_addr}:{src_port}, DST: {dst_addr}:{dst_port}, Priority: {priority}, Size: {payload_length}, Destination not in forwarding table.\n")
-    #         # if the rate permits, proceed to send or drop
-    #         elif (1000*(time.time() - last_send_time) > int(link_params[6])):
-    #             # remove packet from queue
-    #             queue[priority-1].pop(0)
-    #             # send
-    #             if(packet_type == b'E' or randint(1, 100) > int(link_params[7])):
-    #                 socket_obj.sendto(packet, (socket.gethostbyname(link_params[4]), int(link_params[5])))
-    #                 # print(f"[EMULATOR] Sent {packet_type} packet of sequence number {socket.ntohl(seq_num)}")
-    #             # drop
-    #             else:
-    #                 with open(log, 'a') as lf:
-    #                     lf.write(f"[{time.asctime()}] SRC: {src_addr}:{src_port}, DST: {dst_addr}:{dst_port}, Priority: {priority}, Size: {payload_length}, loss event occurred.\n")
-    #                 # print(f"[EMULATOR] Dropped {packet_type} packet of sequence number {socket.ntohl(seq_num)}")
-    #             # log last send time for use with rate
-    #             last_send_time = time.time()
 
     return 0
 
